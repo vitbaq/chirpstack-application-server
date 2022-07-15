dd72e18feff3ee7d51f7b837d9a221c5326ec5c9
miranda <lfms@cesar.org.br>
2022-07-15 20:13:55 -0300

    Refact KNoT register

--- internal/integration/knot/entities/device.go
+++ internal/integration/knot/entities/device.go
@@ -1,29 +1,13 @@
 package entities
 
-// States that represent the current status of the device on the Knot network
-const (
-	KnotNew         string = "new"
-	KnotRegistered         = "registered"
-	KnotDelete             = "delete"
-	KnotForceDelete        = "forceDelete"
-	KnotOk                 = "readToSendData"
-	KnotAuth               = "authenticated"
-	KnotError              = "error"
-	KnotWaitReg            = "waitRegisterResponse"
-	KnotWaitAuth           = "waitAutheResponse"
-	KnotWaitConfig         = "waitConfigResponse"
-	KnotOff                = "ignoreDevice"
-	KnotAlreadyReg         = "alreadyRegistered"
-	KnotWaitUnreg          = "waitUnregister"
-)
-
 // Device represents the device domain entity
 type Device struct {
 	// KNoT Protocol properties
-	ID     string   `mapstructure: "id" json:"id,omitempty"`
-	Token  string   `mapstructure: "token" json:"token,omitempty"`
-	Name   string   `mapstructure: "name" json:"name,omitempty"`
-	Config []Config `mapstructure: "config" json:"config,omitempty"`
+	ID     string   `mapstructure:"id" json:"id,omitempty"`
+	Token  string   `mapstructure:"token" json:"token,omitempty"`
+	DevEUI string   `mapstructure:"deveui" json:"deveui,omitempty"`
+	Name   string   `mapstructure:"name" json:"name,omitempty"`
+	Config []Config `mapstructure:"config" json:"config,omitempty"`
 	State  string   `json:"state,omitempty"`
 	Data   []Data   `json:"data,omitempty"`
 	Error  string
